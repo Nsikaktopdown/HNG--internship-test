d07df377f035e9e338f52c8b0aa0636bb9a4154a
NsikakApp <nsikakthompson73@gmail.com>
2017-08-18 10:08:46 +0100

    inserted data to the table

--- index.php
+++ index.php
@@ -12,39 +12,41 @@ if ($conn->connect_error) {
 } 
 
 
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
+// // sql to create table
+// $sql = "CREATE TABLE MyGuest (
+// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+// firstname VARCHAR(30) NOT NULL,
+// lastname VARCHAR(30) NOT NULL,
+// email VARCHAR(50),
+// reg_date TIMESTAMP
+// )";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
+// if ($conn->query($sql) === TRUE) {
+//     echo "Table MyGuests created successfully";
+  
+// } else {
+//     echo "Error creating table: " . $conn->error;
+// }
 
-function insertData(){
-    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+
+    $sql = "INSERT INTO MyGuest (firstname, lastname, email)
     VALUES ('John', 'Doe', 'john@example.com');";
-    sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+    $sql = "INSERT INTO MyGuest (firstname, lastname, email)
     VALUES ('Mary', 'Moe', 'mary@example.com');";
-    $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+    $sql = "INSERT INTO MyGuest (firstname, lastname, email)
     VALUES ('Julie', 'Dooley', 'julie@example.com')";
 
 if ($conn->multi_query($sql) === TRUE) {
     echo "New records created successfully";
+  
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-}
 
-function queryData(){
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+
+
+$sql = "SELECT id, firstname, lastname FROM MyGuest";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -55,7 +57,7 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-}
+
 
 $conn->close();
 ?>
